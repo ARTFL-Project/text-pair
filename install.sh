ca6c1796ef376846c7c71a2d155b97f0c5a16905
Clovis <clovisgladstone@gmail.com>
2017-12-04 22:51:30 -0600

    update binary file

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 sudo -H pip3 install -r requirements.txt
+sudo pip3 install lib/. --upgrade
 sudo rm -rf /var/lib/text-align
 sudo mkdir -p /var/lib/text-align
 sudo cp -R web /var/lib/text-align/
 sudo cp -R config /var/lib/text-align/
 sudo mkdir -p /etc/text-align
 sudo cp -R web/apache_wsgi.conf /etc/text-align
-echo "Make sure you include /etc/text-align/apache_wsgi.conf in your main Apache configuration file in order to enable searching through the web app."
+echo "\nMake sure you include /etc/text-align/apache_wsgi.conf in your main Apache configuration file in order to enable searching through the web app."
+
 
