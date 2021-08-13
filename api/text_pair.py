abba29fef6d68e59e80bd65ad712c530e231e25f
Clovis Gladstone <clovisgladstone@gmail.com>
2021-08-13 10:31:23 -0500

    style fixes and optimizations

--- api/text_pair.py
+++ api/text_pair.py
@@ -68,14 +68,12 @@ class formArguments:
     def __bool__(self):
         if self.dict:
             return True
-        else:
-            return False
+        return False
 
     def __contains__(self, key):
         if key in self.dict:
             return True
-        else:
-            return False
+        return False
 
     def items(self):
         """Mimic items method of dict"""
@@ -93,10 +91,10 @@ class formArguments:
 
 def get_pg_type(table_name):
     """Find PostgreSQL field type"""
-    cursor = DATABASE.cursor()
-    cursor.execute(f"select * from {table_name}")
-    type_mapping = {23: "INTEGER", 25: "TEXT", 701: "FLOAT"}
-    field_types = {column.name: type_mapping[column.type_code] for column in cursor.description}
+    with DATABASE.cursor() as cursor:
+        cursor.execute(f"select * from {table_name}")
+        type_mapping = {23: "INTEGER", 25: "TEXT", 701: "FLOAT"}
+        field_types = {column.name: type_mapping[column.type_code] for column in cursor.description}
     return field_types
 
 
@@ -160,38 +158,38 @@ def query_builder(query_args, other_args, field_types):
                     value = regular_query
                 if value.startswith('"'):
                     if value == '""':
-                        query = "{} = %s".format(field)
+                        query = f"{field} = %s"
                         sql_values.append("")
                     else:
-                        query = "{}=%s".format(field)
+                        query = f"{field}=%s"
                         sql_values.append(value[1:-1])
                 elif value.startswith("NOT "):
                     split_value = " ".join(value.split()[1:]).strip()
-                    query = "{} !~* %s".format(field)
-                    sql_values.append("\m{}\M".format(split_value))
+                    query = f"{field} !~* %s"
+                    sql_values.append(f"\m{split_value}\M")
                 # elif value.startswith("OR "):  ## TODO: add support to OR queries by changing the join logic at the end of the function
                 #     split_value = " ".join(value.split()[1:]).strip()
                 #     query = "{} !~* %s".format(field)
                 #     sql_values.append('\m{}\M'.format(split_value))
                 else:
-                    query = "{} ~* %s".format(field)
-                    sql_values.append("\m{}\M".format(value))
+                    query = f"{field} ~* %s"
+                    sql_values.append(f"\m{value}\M")
                 sql_fields.append(query)
-        elif field_type == "INTEGER" or field_type == "FLOAT":
+        elif field_type in ("INTEGER", "FLOAT"):
             value = value.replace('"', "")
             if "-" in value:
                 values = [v for v in re.split(r"(-)", value) if v]
                 if values[0] == "-":
-                    query = "{} <= %s".format(field)
+                    query = f"{field} <= %s"
                     sql_values.append(values[1])
                 elif values[-1] == "-":
-                    query = "{} >= %s".format(field)
+                    query = f"{field} >= %s"
                     sql_values.append(values[0])
                 else:
-                    query = "{} BETWEEN %s AND %s".format(field)
+                    query = f"{field} BETWEEN %s AND %s"
                     sql_values.extend([values[0], values[2]])
             else:
-                query = "{} = %s".format(field)
+                query = f"{field} = %s"
                 sql_values.append(value)
             sql_fields.append(query)
         else:
@@ -232,51 +230,42 @@ def index(db_path: str):
     return HTMLResponse(index_html)
 
 
-# @app.get("/{db_path}/search_alignments/")
 @app.post("/search_alignments/")
 def search_alignments(request: Request, metadata: Dict[str, str]):
     """Search alignments according to URL params"""
     sql_fields, sql_values, other_args, column_names = parse_args(request)
     if other_args.direction == "next":
         if sql_fields:
-            query = "SELECT o.rowid_ordered, m.* FROM {} m, {}_ordered o WHERE {} AND o.source_year_target_year=m.rowid and \
-                    o.rowid_ordered > {} ORDER BY o.rowid_ordered LIMIT 50".format(
-                other_args.db_table, other_args.db_table, sql_fields, other_args.id_anchor
-            )
+            query = f"SELECT o.rowid_ordered, m.* FROM {other_args.db_table} m, {other_args.db_table}_ordered o WHERE {sql_fields} AND o.source_year_target_year=m.rowid and \
+                    o.rowid_ordered > {other_args.id_anchor} ORDER BY o.rowid_ordered LIMIT 50"
         else:
-            query = "SELECT o.rowid_ordered, m.* FROM {} m, {}_ordered o WHERE o.source_year_target_year=m.rowid and \
-                    o.rowid_ordered > {} ORDER BY o.rowid_ordered LIMIT 50".format(
-                other_args.db_table, other_args.db_table, other_args.id_anchor
-            )
+            query = f"SELECT o.rowid_ordered, m.* FROM {other_args.db_table} m, {other_args.db_table}_ordered o WHERE o.source_year_target_year=m.rowid and \
+                    o.rowid_ordered > {other_args.id_anchor} ORDER BY o.rowid_ordered LIMIT 50"
     else:
         if sql_fields:
-            query = "SELECT o.rowid_ordered, m.* FROM {} m, {}_ordered o WHERE {} AND o.source_year_target_year=m.rowid and \
-                    o.rowid_ordered < {} ORDER BY o.rowid_ordered desc LIMIT 50".format(
-                other_args.db_table, other_args.db_table, sql_fields, other_args.id_anchor
-            )
+            query = f"SELECT o.rowid_ordered, m.* FROM {other_args.db_table} m, {other_args.db_table}_ordered o WHERE {sql_fields} AND o.source_year_target_year=m.rowid and \
+                    o.rowid_ordered < {other_args.id_anchor} ORDER BY o.rowid_ordered desc LIMIT 50"
         else:
-            query = "SELECT o.rowid_ordered, m.* FROM {} m, {}_ordered o WHERE o.source_year_target_year=m.rowid and \
-                    o.rowid_ordered < {} ORDER BY o.rowid_ordered desc LIMIT 50".format(
-                other_args.db_table, other_args.db_table, other_args.id_anchor
-            )
-    cursor = DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(query, sql_values)
-    alignments = []
-    for row in cursor:
-        metadata = {key: row[key] for key in column_names}
-        metadata["rowid_ordered"] = row["rowid_ordered"]
-        alignments.append(metadata)
+            query = f"SELECT o.rowid_ordered, m.* FROM {other_args.db_table} m, {other_args.db_table}_ordered o WHERE o.source_year_target_year=m.rowid and \
+                    o.rowid_ordered < {other_args.id_anchor} ORDER BY o.rowid_ordered desc LIMIT 50"
+    with DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(query, sql_values)
+        alignments = []
+        for row in cursor:
+            metadata = {key: row[key] for key in column_names}
+            metadata["rowid_ordered"] = row["rowid_ordered"]
+            alignments.append(metadata)
     if other_args.direction == "previous":
         alignments.reverse()
     previous_url = ""
     current_path = re.sub(r"&(page|id_anchor|direction)=(previous|next|\d*)", "", request.url.path)
     if other_args.page > 1:
-        previous_url = "{}&page={}&id_anchor={}&direction=previous".format(
-            current_path, other_args.page - 1, alignments[0]["rowid_ordered"]
+        previous_url = (
+            f"{current_path}&page={other_args.page - 1}&id_anchor={alignments[0]['rowid_ordered']}&direction=previous"
         )
     try:
-        next_url = "{}&page={}&id_anchor={}&direction=next".format(
-            current_path, other_args.page + 1, alignments[-1]["rowid_ordered"]
+        next_url = (
+            f"{current_path}&page={other_args.page + 1}&id_anchor={alignments[-1]['rowid_ordered']}&direction=next"
         )
     except IndexError:
         next_url = ""
@@ -300,16 +289,6 @@ def retrieve_all(request: Request):
         direction = "source_"
     else:
         direction = "target_"
-    cursor = DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    if sql_values:
-        query = (
-            f"""SELECT * FROM {other_args.db_table} WHERE {other_args.field}='{other_args.value}' AND {sql_fields}"""
-        )
-        cursor.execute(query, sql_values)
-    else:
-        query = f"""SELECT * FROM {other_args.db_table} WHERE {other_args.field}='{other_args.value}'"""
-        cursor.execute(query)
-
     filtered_columns = {
         "source_filename",
         "source_passage",
@@ -332,11 +311,17 @@ def retrieve_all(request: Request):
     ]
     docs_found = {}
     doc_id = f"{direction}doc_id"
-    for row in cursor:
-        if row[doc_id] not in docs_found:
-            docs_found[row[doc_id]] = {"count": 0, **{key: row[key] for key in column_names}}
-        docs_found[row[doc_id]]["count"] += 1
-
+    with DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        if sql_values:
+            query = f"""SELECT * FROM {other_args.db_table} WHERE {other_args.field}='{other_args.value}' AND {sql_fields}"""
+            cursor.execute(query, sql_values)
+        else:
+            query = f"""SELECT * FROM {other_args.db_table} WHERE {other_args.field}='{other_args.value}'"""
+            cursor.execute(query)
+        for row in cursor:
+            if row[doc_id] not in docs_found:
+                docs_found[row[doc_id]] = {"count": 0, **{key: row[key] for key in column_names}}
+            docs_found[row[doc_id]]["count"] += 1
     return list(docs_found.values())
 
 
@@ -357,114 +342,100 @@ def retrieve_all_passage_pairs(request: Request):
     }
     column_names = [column_name for column_name in column_names if column_name not in filtered_columns]
     query = f"SELECT * FROM {other_args.db_table} WHERE {sql_fields}"
-    cursor = DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(query, sql_values)
-    results = [{key: row[key] for key in column_names} for row in cursor]
+    with DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(query, sql_values)
+        results = [{key: row[key] for key in column_names} for row in cursor]
     return results
 
 
-# @app.get("/count_results/")
 @app.post("/count_results/")
 def count_results(request: Request):
     """Search alignments according to URL params"""
     sql_fields, sql_values, other_args, _ = parse_args(request)
     if sql_fields:
-        query = "SELECT COUNT(*) FROM {} WHERE {}".format(other_args.db_table, sql_fields)
+        query = f"SELECT COUNT(*) FROM {other_args.db_table} WHERE {sql_fields}"
     else:
-        query = "SELECT COUNT(*) FROM {}".format(other_args.db_table)
-    cursor = DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(query, sql_values)
-    result_object = {"counts": cursor.fetchone()[0]}
+        query = f"SELECT COUNT(*) FROM {other_args.db_table}"
+    with DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(query, sql_values)
+        result_object = {"counts": cursor.fetchone()[0]}
     return result_object
 
 
-# @app.get("/generate_time_series/")
 @app.post("/generate_time_series/")
 def generate_time_series(request: Request):
     """Generate a time series from search results"""
     # TODO: don't assume year is the field to use
     sql_fields, sql_values, other_args, _ = parse_args(request)
     if sql_fields:
-        query = "select interval AS year, COUNT(*) FROM \
-                (SELECT floor({}_year/{})*{} AS interval FROM {} WHERE {}) t \
-                GROUP BY interval ORDER BY interval".format(
-            other_args.directionSelected,
-            other_args.timeSeriesInterval,
-            other_args.timeSeriesInterval,
-            other_args.db_table,
-            sql_fields,
-        )
+        query = f"select interval AS year, COUNT(*) FROM \
+                (SELECT floor({other_args.directionSelected}_year/{other_args.timeSeriesInterval})*{other_args.timeSeriesInterval} \
+                AS interval FROM {other_args.db_table} WHERE {sql_fields}) t \
+                GROUP BY interval ORDER BY interval"
     else:
-        query = "select interval AS year, COUNT(*) FROM \
-                (SELECT floor({}_year/{})*{} AS interval FROM {}) t \
-                GROUP BY interval ORDER BY interval".format(
-            other_args.directionSelected,
-            other_args.timeSeriesInterval,
-            other_args.timeSeriesInterval,
-            other_args.db_table,
-        )
-    cursor = DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(query, sql_values)
+        query = f"select interval AS year, COUNT(*) FROM \
+                (SELECT floor({other_args.directionSelected}_year/{other_args.timeSeriesInterval})*{other_args.timeSeriesInterval} \
+                AS interval FROM {other_args.db_table}) t \
+                GROUP BY interval ORDER BY interval"
     results = []
     total_results = 0
     next_year = None
-    for year, count in cursor:
-        if year is None:
-            continue
-        if next_year is not None:
-            while year > next_year:
-                results.append({"year": next_year, "count": 0})
-                next_year += other_args.timeSeriesInterval
-        results.append({"year": year, "count": count})
-        next_year = year + other_args.timeSeriesInterval
-        total_results += count
+    with DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(query, sql_values)
+        for year, count in cursor:
+            if year is None:
+                continue
+            if next_year is not None:
+                while year > next_year:
+                    results.append({"year": next_year, "count": 0})
+                    next_year += other_args.timeSeriesInterval
+            results.append({"year": year, "count": count})
+            next_year = year + other_args.timeSeriesInterval
+            total_results += count
     return {"counts": total_results, "results": results}
 
 
-# @app.get("/facets/")
 @app.post("/facets/")
 def facets(request: Request):
     """Retrieve facet result"""
     sql_fields, sql_values, other_args, _ = parse_args(request)
-    cursor = DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    if sql_fields:
-        query = "SELECT {}, COUNT(*) FROM {} WHERE {} GROUP BY {} ORDER BY COUNT(*) DESC".format(
-            other_args.facet, other_args.db_table, sql_fields, other_args.facet
-        )
-    else:
-        query = "SELECT {}, COUNT(*) FROM {} GROUP BY {} ORDER BY COUNT(*) DESC".format(
-            other_args.facet, other_args.db_table, other_args.facet
-        )
-    cursor.execute(query, sql_values)
     results = []
     total_count = 0
-    if not other_args.facet.endswith("passage_length"):
-        for result in cursor:
-            field_name, count = result
-            results.append({"field": field_name, "count": count})
-            total_count += count
-    else:
-        counts = Counter()
-        for length, count in cursor:
-            if length < 26:
-                counts["1-25"] += count
-            if 25 < length < 101:
-                counts["26-100"] += count
-            elif 100 < length < 251:
-                counts["101-250"] += count
-            elif 250 < length < 501:
-                counts["251-500"] += count
-            elif 500 < length < 1001:
-                counts["501-1000"] += count
-            elif 1000 < length < 3001:
-                counts["1001-3000"] += count
-            elif length > 3000:
-                counts["3001-"] += count
-            total_count += count
-        results = [
-            {"field": interval, "count": count}
-            for interval, count in sorted(counts.items(), key=lambda x: x[1], reverse=True)
-        ]
+    with DATABASE.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        if sql_fields:
+            query = f"SELECT {other_args.facet}, COUNT(*) FROM {other_args.db_table} \
+                WHERE {sql_fields} GROUP BY {other_args.facet} ORDER BY COUNT(*) DESC"
+        else:
+            query = f"SELECT {other_args.facet}, COUNT(*) FROM {other_args.db_table} \
+                GROUP BY {other_args.facet} ORDER BY COUNT(*) DESC"
+        cursor.execute(query, sql_values)
+        if not other_args.facet.endswith("passage_length"):
+            for result in cursor:
+                field_name, count = result
+                results.append({"field": field_name, "count": count})
+                total_count += count
+        else:
+            counts = Counter()
+            for length, count in cursor:
+                if length < 26:
+                    counts["1-25"] += count
+                if 25 < length < 101:
+                    counts["26-100"] += count
+                elif 100 < length < 251:
+                    counts["101-250"] += count
+                elif 250 < length < 501:
+                    counts["251-500"] += count
+                elif 500 < length < 1001:
+                    counts["501-1000"] += count
+                elif 1000 < length < 3001:
+                    counts["1001-3000"] += count
+                elif length > 3000:
+                    counts["3001-"] += count
+                total_count += count
+            results = [
+                {"field": interval, "count": count}
+                for interval, count in sorted(counts.items(), key=lambda x: x[1], reverse=True)
+            ]
     return {"facet": other_args.facet, "results": results, "total_count": total_count}
 
 
