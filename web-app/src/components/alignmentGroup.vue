51cc5ace96713a8b9459b2bf4c3a5c67ce74efa9
Clovis Gladstone <clovisgladstone@gmail.com>
2023-04-12 11:29:06 -0500

    fix centering of modal in passage group

--- web-app/src/components/alignmentGroup.vue
+++ web-app/src/components/alignmentGroup.vue
@@ -42,8 +42,8 @@
             </div>
         </div>
         <div id="passage-diff" class="modal fade" tabindex="-1">
-            <div class="modal-dialog">
-                <div class="modal-content" style="min-width: 1024px">
+            <div class="modal-dialog" style="min-width: 1024px">
+                <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Modal title</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
