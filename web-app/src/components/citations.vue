32829d6f3e8d5c990472e083db118527f382b49d
Clovis Gladstone <clovisgladstone@gmail.com>
2026-01-26 18:53:51 -0600

    Optimize Docker image: add .dockerignore, use CPU-only torch, fix source_against_source in citations

--- web-app/src/components/citations.vue
+++ web-app/src/components/citations.vue
@@ -1,10 +1,10 @@
 <template>
     <p class="pt-2 mb-2 px-3">
         <span class="citation" v-for="(cite, citeIndex) in citation" :key="citeIndex">
-            <router-link :to="href" :style="cite.style" v-if="linkToDoc == cite.field">{{ alignment[cite.field]
+            <router-link :to="href" :style="cite.style" v-if="linkToDoc == cite.field && getFieldValue(cite.field)">{{ getFieldValue(cite.field)
             }}</router-link>
-            <span :style="cite.style" v-else v-if="alignment[cite.field]">{{ alignment[cite.field] }}</span>
-            <span class="separator px-2" v-if="alignment[cite.field] && citeIndex != citation.length - 1">&#9679;</span>
+            <span :style="cite.style" v-else v-if="getFieldValue(cite.field)">{{ getFieldValue(cite.field) }}</span>
+            <span class="separator px-2" v-if="getFieldValue(cite.field) && citeIndex != citation.length - 1">&#9679;</span>
         </span>
     </p>
 </template>
@@ -17,6 +17,20 @@ export default {
     data() {
         return {
             globalConfig: this.$globalConfig
+        }
+    },
+    methods: {
+        getFieldValue(field) {
+            // If field exists, return it
+            if (this.alignment[field]) {
+                return this.alignment[field];
+            }
+            // If target field doesn't exist (source_against_source case), try source equivalent
+            if (field.startsWith('target_')) {
+                const sourceField = field.replace('target_', 'source_');
+                return this.alignment[sourceField];
+            }
+            return null;
         }
     },
     computed: {
